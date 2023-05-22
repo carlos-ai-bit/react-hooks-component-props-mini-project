8529f3c7d1364bb1f0579d06798875abd34b82ae
Hoperaisers <abdallahsafari@gmail.com>
2023-05-22 17:11:35 +0300

    initial commit

---
+++ src/components/Article.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Article() {
+
+  return <h1>Colorado State Parks!</h1>;
+}
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import howManyParks from "./parks/howManyParks";
+
+function ArticleList() {
+
+    return <main><h1>The WET Codebase</h1>
+        <p>July13,2020.   1 min read</p>
+        <h4>Come waste your time with me</h4>
+        </main>
+}
+export default ArticleList;
